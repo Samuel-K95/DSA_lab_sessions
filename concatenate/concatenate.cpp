8de0c98087110f4433bd1aa4a8289cea5dff27c2
Samuel Keno <sgere195@gmail.com>
2023-04-11 16:40:43 +0300

    Assignment on linked list and stack

--- concatenate/concatenate.cpp
+++ concatenate/concatenate.cpp
@@ -1,45 +1,52 @@
-#include <iostream>
-#include<bits/stdc++.h>
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node *next;
-};
-
-class NodeOperation{
-  public:
-
-    // Function to add a new node
-    void pushNode(class Node** head_ref,int data_val){
-
-        // Allocate node
-        class Node *new_node = new Node();
-
-         // Put in the data
-        new_node->data = data_val;
-
-        // Link the old list of the new node
-        new_node->next = *head_ref;
-
-        // move the head to point to the new node
-        *head_ref = new_node;
-    }
-};
-
-int main() {
-class Node *head = NULL;
-    class NodeOperation *temp = new NodeOperation();
-    for(int i=5; i>0; i--){
-        temp->pushNode(&head, i);
-    }
-    vector<int>v;
-    while(head!=NULL){
-     v.push_back(head->data);
-     head=head->next;
-    }
-    cout<<"Middle Value Of Linked List is :";
-    cout<<v[v.size()/2]<<endl;
-    return 0;
-}
+const int ROWS = 10;
+const int COLS = 10;
+const int NUM_MINES = 20;// function to initialize the board
+void init(int board[][COLS]) {    // fill the board with zeros
+    for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                board[r][c] = 0;        }    }    // randomly place mines
+int mines_placed = 0;
+while (mines_placed < NUM_MINES) {
+        int r = rand() % ROWS;
+int c = rand() % COLS;
+if (board[r][c] == 0) {
+        board[r][c] = -1;
+mines_placed++;        }    }    // fill in the rest of the cells with numbers
+for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            if (board[r][c] != -1) {
+                int count = 0;
+if (r > 0 && c > 0 && board[r-1][c-1] == -1) count++; // top left
+if (r > 0 && board[r-1][c] == -1) count++; // top
+if (r > 0 && c < COLS-1 && board[r-1][c+1] == -1) count++; // top right
+if (c > 0 && board[r][c-1] == -1) count++; // left
+ if (c < COLS-1 && board[r][c+1] == -1) count++; // right
+ if (r < ROWS-1 && c > 0 && board[r+1][c-1] == -1) count++; // bottom left
+ if (r < ROWS-1 && board[r+1][c] == -1) count++; // bottom
+ if (r < ROWS-1 && c < COLS-1 && board[r+1][c+1] == -1) count++; // bottom right
+  board[r][c] = count;            }        }    }}// function to print the board
+  void printBoard(int board[][COLS], bool reveal = false)
+  {    cout << "  ";
+   for (int c = 0; c < COLS; c++) {
+                cout << c << " ";    }
+   cout << endl;
+   for (int r = 0; r < ROWS; r++) {
+        cout << r << " ";
+   for (int c = 0; c < COLS; c++) {
+        if (board[r][c] == -1 && reveal) { // show mines
+            cout << "* ";            }
+   else if (board[r][c] >= 0) { // show numbers
+        cout << board[r][c] << " ";            }
+   else {                cout << ". "; // show unexplored cells
+   }        }        cout << endl;    }}// function to play the game
+   void play(int board[][COLS]) {
+        bool gameOver = false;
+         while (!gameOver) {
+                printBoard(board);
+         int r, c;
